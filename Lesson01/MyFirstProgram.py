69436b05bce4e219c500652fc825f651627136f1
Xi Zuo <36340668+pangmi@users.noreply.github.com>
2021-12-12 21:26:22 -0800

    Create MyFirstProgram.py

---
+++ Lesson01/MyFirstProgram.py
@@ -0,0 +1,20 @@
+# MyFirstProgram.py
+
+print("Hello world")
+
+name = "My name is Python."
+print(name)
+
+x = 47
+print("x = ", x)
+
+y = 9
+print("y = ", y)
+      
+print("x + y =", x + y)
+print("x - y =", x - y)
+print("x * y =", x * y)
+print("x / y =", x / y)
+print("x % y =", x % y)
+print("x // y =", x // y)
+print("y ** 3 =", y ** 3)
