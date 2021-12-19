eb11551fcb6efdf58b841b7644d7e52d80d882e0
Xi Zuo <36340668+pangmi@users.noreply.github.com>
2021-12-19 11:26:57 -0800

    Create MyStringProgram.py

---
+++ Lesson02/Code/MyStringProgram.py
@@ -0,0 +1,6 @@
+str1 = 'Hello'
+str2 = "Hello"
+str3 = "Peter is Clara's dog"
+str4 = 'A wise said: "Programmers are optimists"'
+print(str1, str2, str3, str4)
+print(str1, str2, str3, str4, sep='|')
