1ed2be7b954358d8df0d6bb9fbec00027b074d51
Xi Zuo <36340668+pangmi@users.noreply.github.com>
2021-12-26 20:18:01 -0800

    Create YourName.py

---
+++ Lesson03/Code/YourName.py
@@ -0,0 +1,5 @@
+# input() takes input from user with a prompting message, and returns the
+# user input as a string
+
+name = input('Please enter your name: ')
+print("Hello", name, name, name, name, name)
