e679dc0d4d4d4ec338b07780b51e63a2b8d69925
Xi Zuo <36340668+pangmi@users.noreply.github.com>
2021-12-26 20:15:20 -0800

    Create MyListProgram.py

---
+++ Lesson03/Code/MyListProgram.py
@@ -0,0 +1,24 @@
+# Variable names should be lowercase, with words separated by underscores as necessary
+
+# the following are good variable names:
+myage = 7
+my_age = 7
+
+# the following are not good pratice:
+Myage = 7
+myAge = 7
+MyAge = 7
+
+# List is a collection of items
+numbers = [32, 1, 45, 60]
+even_numbers = [2, 4, 6, 8, 12]
+strs = ['aa', 'bb', 'cc']
+cars = ["Ford Mustang", "Chevy Malibu", "BMW X5", "Toyata Camry", "Tesla 3"]
+coders = ['Kingston', 'Felix', 'Clara']
+
+# use index to access items in list (index is 0 based)
+some_coder = coders[1]
+print(some_coder)
+print(len(coders))
+print(coders)
+print(coders[0], coders[1], coders[2])
