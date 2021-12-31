dab35cf719e806de1fbdfc9ee98d92b0f111993f
Xi Zuo <36340668+pangmi@users.noreply.github.com>
2021-12-31 11:26:44 -0800

    Create MySquare1.py

---
+++ Lesson03/Code/MySquare1.py
@@ -0,0 +1,14 @@
+import turtle
+
+turtle.bgcolor('gray')
+
+t = turtle.Pen()
+t.pencolor('blue')
+
+t.forward(200)
+t.left(90)
+t.forward(200)
+t.left(90)
+t.forward(200)
+t.left(90)
+t.forward(200)
